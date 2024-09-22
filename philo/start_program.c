46652925728deeff0b94d4ecba18061e0225629c
tranhieutrung <tranhieutrung@outlook.com>
2024-09-22 13:08:33 +0300

    Refactored the code and added protections.

--- philo/start_program.c
+++ philo/start_program.c
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 21:38:43 by hitran            #+#    #+#             */
-/*   Updated: 2024/09/19 22:31:50 by hitran           ###   ########.fr       */
+/*   Updated: 2024/09/22 13:07:36 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,21 @@ bool	start_program(t_philo *philo)
 
 	threads = (pthread_t *)ft_calloc(philo->args.num_of_philos, sizeof(pthread_t));
 	if (!threads)
-		return (error_thread(philo));
-	pthread_create(&watcher, NULL, &death_watcher, (void *)philo);
-	pthread_detach(watcher);
-	i = -1;
-	while (++i < philo->args.num_of_philos)
-	{
-		pthread_create(&threads[i], NULL, &routine, (void *)philo);
-		philo = philo->next;
-	}
+		return (error_thread(philo)); //handle error
+	if (pthread_create(&watcher, NULL, &death_watcher, (void *)philo))
+		return (error_thread(philo)); //handle error
+	if (pthread_detach(watcher))
+		return (error_thread(philo)); //handle error
 	i = 0;
 	while (i < philo->args.num_of_philos)
-		pthread_join(threads[i++], NULL);
+	{
+		if (pthread_create(&threads[i], NULL, &routine, (void *)philo))
+			return (error_thread(philo)); //handle error
+		philo = philo->next;
+		if (pthread_join(threads[i], NULL))
+			return (error_thread(philo)); //handle error
+		i++;
+	}
 	free(threads);
 	cleaner(philo);
 	return (true);
