ad6250cdc218348558ba4789f14ffcbcae52255e
Hieu Trung Tran <hitran@c1r3p3.hive.fi>
2024-09-25 15:33:10 +0300

    Modified the structure, code. The init_mutexes does not work.

---
+++ philo/parse_input.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 10:32:34 by hitran            #+#    #+#             */
+/*   Updated: 2024/09/25 15:17:06 by hitran           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static bool arg_error(char *argv, char *message)
+{
+	ft_putstr_fd(2, "Error: ");
+	if (argv)
+	{
+		ft_putstr_fd(2, argv);
+		ft_putstr_fd(2, ": ");
+	}
+	ft_putstr_fd(2, message);
+	ft_putstr_fd(2, "\n");
+	return (false);
+}
+
+static long	ft_atopi(char *s)
+{
+	long	number;
+
+	if (*s == '+')
+		s++;
+	number = 0;
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+			return (-1);
+		number = (number * 10) + (*s - '0');
+		if (number > MAX_INT)
+			return (-1);
+		s++;
+	}
+	return (number);
+}
+
+bool	is_valid_args(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc < 5 || argc > 6)
+		return (arg_error(NULL, "Invalid number of arguments"));
+	while (++i < argc)
+	{
+		if (ft_atopi(argv[i]) <= 0)
+			return (arg_error(argv[i], "Is invalid"));
+	}
+	return (true);
+}
+
+bool	parse_input(t_philo *philo, int argc, char **argv)
+{
+	if (!is_valid_args(argc, argv))
+		return(false);
+	philo->num_of_philos = ft_atopi(argv[1]);
+	philo->time_to_die = ft_atopi(argv[2]);
+	philo->time_to_eat = ft_atopi(argv[3]);
+	philo->time_to_sleep = ft_atopi(argv[4]);
+	if (argv[5])
+		philo->num_of_meals = ft_atopi(argv[5]);
+	else
+		philo->num_of_meals = -1;
+	return (true);
+}
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 22:11:41 by hitran            #+#    #+#             */
-/*   Updated: 2024/09/22 09:31:52 by hitran           ###   ########.fr       */
+/*   Updated: 2024/09/25 15:29:24 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,54 +19,69 @@
 # include <stdio.h>			// printf
 # include <sys/time.h>		// gettimeofday
 # include <stdbool.h>		// bool
+# include <string.h>		// memset
 
 # define MAX_INT 2147483647
 
-typedef struct s_arg
-{
-	int	num_of_philos;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	num_of_meals;
-} t_arg;
+// typedef struct s_arg
+// {
+// 	int	num_of_philos;
+// 	int	time_to_die;
+// 	int	time_to_eat;
+// 	int	time_to_sleep;
+// 	int	num_of_meals;
+// } t_arg;
 
-typedef struct s_mutex
-{
-	int	stop;
-	int	eaten;
-	pthread_mutex_t	stop;
-	pthread_mutex_t	eaten;
-	pthread_mutex_t	print;
-} t_mutex;
+// typedef struct s_mutex
+// {
+// 	int	stop;
+// 	int	eaten;
+// 	pthread_mutex_t	finish;
+// 	pthread_mutex_t	eaten;
+// 	pthread_mutex_t	print;
+// } t_mutex;
 
-typedef struct s_status
+typedef struct s_thread
 {
-	int	ate;
-	int	died;
-	int	finish;
+	pthread_t		thread;
+	size_t			id;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+} t_thread;
+
+typedef enum e_status
+{
+	ATE,
+	DIED,
+	FINISH
 } t_status;
 
 typedef struct s_philo
 {
-	int				id;
-	int				num_of_chopsticks;
-	t_arg			args;
-	t_mutex			*mutexes;
+	size_t			num_of_philos;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			num_of_meals;
+	pthread_mutex_t	**forks;
+	pthread_mutex_t	*lock;
+	t_thread		*threads;
 	t_status		status;
-	pthread_mutex_t	chopstick;
-	struct s_philo	*next;
+	long			millisecond;
 } t_philo;
 
 //----------------------------||     PHILO      ||----------------------------//
-
-bool	init_philos(t_philo *philo, char **argv);
+bool	parse_input(t_philo *philo, int argc, char **argv);
+bool	init_philos(t_philo *philo);
 bool	start_program(t_philo *philo);
 
 //----------------------------||     UTILS      ||----------------------------//
 
-size_t	ft_strlen(const char *s);
-long	ft_atol(const char *s);
-void	*ft_calloc(size_t count, size_t size);
+// size_t	ft_strlen(const char *s);
+// long	ft_atol(const char *s);
+// void	*ft_calloc(size_t count, size_t size);
+void	ft_putstr_fd(int fd, char *s);
+long	get_time(void);
+bool	free_philo(t_philo *philo);
 
 #endif
